0fb8ba22583323492e4fc368497622428408b06b
Cyril Tan <cyril.tan@digitecgalaxus.ch>
2023-10-19 11:30:25 +0200

    hardcoded newer kernel version

--- build/kernel/install-kernel-packages.sh
+++ build/kernel/install-kernel-packages.sh
@@ -9,12 +9,12 @@ else
         netbootIP="$1"
 fi
 
-kernelVersion=$(curl -s --connect-timeout 2 "http://$netbootIP/kernels/latest-kernel-version.json" | jq -r .version)
+kernelVersion="linux-image-uc-6.5.0-9-generic" # $(curl -s --connect-timeout 2 "http://$netbootIP/kernels/latest-kernel-version.json" | jq -r .version)
 
-if [[ "$kernelVersion" == "" ]]
-then
-  kernelVersion="6.2.0-20-generic"
-  echo "Warning: Using fallback static kernel version $kernelVersion"
-fi
+# if [[ "$kernelVersion" == "" ]]
+# then
+#   kernelVersion="6.2.0-20-generic"
+#   echo "Warning: Using fallback static kernel version $kernelVersion"
+# fi
 
-apt-get -qq update > /dev/null 2>&1 && apt-get -qq install -y "linux-image-$kernelVersion" "linux-modules-extra-$kernelVersion" > /dev/null 2>&1
+# apt-get -qq update > /dev/null 2>&1 && apt-get -qq install -y "linux-image-$kernelVersion" "linux-modules-extra-$kernelVersion" > /dev/null 2>&1
